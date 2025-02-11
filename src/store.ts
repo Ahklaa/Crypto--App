046d9a63ede3ebd3fef5f1dfe87ee0da3908f4dc
Ahkla <ahkla185@hotmail.com>
2025-02-11 12:12:06 -0500

    se agrego la peticion de la api en una carpeta de services y en el archivo Cryptoservices para que el codigo no sea tan largo

--- src/store.ts
+++ src/store.ts
@@ -1,21 +1,13 @@
 import { create } from "zustand"
 import { devtools } from "zustand/middleware"
-import axios from 'axios'
-import { CurrenciesCryptoResponseSchema } from "./schema/crypto-schema"
 import { CryptoCurrency } from "./types"
+import { getCryptos } from "./service/CryptoServices"
 
 type CryptoStore = {
     cryptoCurrencies : CryptoCurrency[],
     fetchCrypto: () => Promise<void>
 }
-async function getCryptos(){
-    const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD"
-    const {data: {Data}} = await axios.get(url)
-    const response = CurrenciesCryptoResponseSchema.safeParse(Data)
-    if(response.success){
-        return response.data
-    }
-}
+
 export const useCryptoStore = create<CryptoStore>()(devtools((set) => ({
     cryptoCurrencies : [],
     fetchCrypto : async() => {
