09a41de44bb3c1d7b5fc967311224eb6b4467f11
Ahkla <ahkla185@hotmail.com>
2025-02-11 11:54:40 -0500

    finalizando la funcion que obtiene las criptos,ademas creando un type infiriendolo del cryptoSchema para luego usarlo en el useCryptoStore para saber que datos trae

--- src/store.ts
+++ src/store.ts
@@ -1,15 +1,27 @@
 import { create } from "zustand"
 import axios from 'axios'
 import { CurrenciesCryptoResponseSchema } from "./schema/crypto-schema"
+import { CryptoCurrency } from "./types"
+
+type CryptoStore = {
+    cryptoCurrencies : CryptoCurrency[],
+    fetchCrypto: () => Promise<void>
+}
 async function getCryptos(){
     const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD"
     const {data: {Data}} = await axios.get(url)
     const response = CurrenciesCryptoResponseSchema.safeParse(Data)
-    console.log(response);
-    
+    if(response.success){
+        return response.data
+    }
 }
-export const useCryptoStore = create(() => ({
-    fetchCrypto : () => {
-        getCryptos()
+export const useCryptoStore = create<CryptoStore>((set) => ({
+    cryptoCurrencies : [],
+    fetchCrypto : async() => {
+        const cryptoCurrencies = await getCryptos()
+        set(()=> ({
+            cryptoCurrencies
+        }))
+        
     }
 }))
